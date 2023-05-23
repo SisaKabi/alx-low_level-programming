f94b0527d6184aeedea7e88615ad1be8e963dd4c
Busisa Kabi <sisakabi@icloud.com>
2023-05-23 14:00:00 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _islower - testsif the character is lowercase
+ * _islower - tests if the character is lowercase
+ * @c: character to be tested
  *
  * Return: 1 if lowercase, 0 if otherwise
  */
@@ -10,8 +11,6 @@ int _islower(int c)
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
-	}else 
-	{
-		return (0);
 	}
+		return (0);
 }
