ea2f9cb6009581d3e503bf70e5fb247644259339
Busisa Kabi <sisakabi@icloud.com>
2023-05-23 15:47:46 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - finds and prints the last digit to stdout
+ *@b: number to be evaluated. 
+ *
+ * Return: Last digit.
+ */
+int print_last_digit(int b)
+{
+	int last;
+
+	last = b % 10;
+
+	return (last);
+}
