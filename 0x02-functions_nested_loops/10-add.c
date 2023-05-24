49e3235d2739c3fbad3f8aff38c7c1d9d9a704c4
Busisa Kabi <sisakabi@icloud.com>
2023-05-23 19:32:24 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds the two integers together
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: Result of sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
