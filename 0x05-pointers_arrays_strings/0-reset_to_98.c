7a25fbc7e6889aee9627a73ad7df43e1ae1694f3
Busisa Kabi <sisakabi@icloud.com>
2023-05-29 08:01:26 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the given value to 98.
+ * @n: number to be reset
+ *
+ * Return: Implicit
+ */
+void reset_to_98(int *n)
+{
+	int *p = n;
+
+	*p = 98;
+}
