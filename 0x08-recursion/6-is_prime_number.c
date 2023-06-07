235efe6e1bb6e09716cd1104c1151f6b1900551d
Busisa Kabi <sisakabi@icloud.com>
2023-06-07 16:13:28 -0700

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,10 @@
  */
 int is_prime_number(int n)
 {
-	if (n == 1)	
+	if (n == 1)
+	{
+		return (0);
+	} else if (n <= 0)
 	{
 		return (0);
 	} else if (n % 2 == 0 && n != 2)
@@ -20,6 +23,6 @@ int is_prime_number(int n)
 	} else if (n % 5 == 0 && n != 5)
 	{
 		return (0);
-	} else 
+	} else
 		return (1);
 }
