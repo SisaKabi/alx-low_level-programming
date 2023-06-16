5b86fa5e1ac9fab95bcc4b82afc6fde7a6feb994
Busisa Kabi <sisakabi@icloud.com>
2023-06-15 17:25:07 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,31 +6,34 @@
  * string_nconcat - concatenates two strings and returns a pointer
  * @s1: first string
  * @s2: second string
- * @n: number of characters in one string.
+ * @n: number of characters in s2 to print.
  *
  * Return: pointer to new string on succes. NULL on failure.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int len1;
+	int len;
 	unsigned int i;
 	char *ptr, *concat;
 
 	if (s1 != NULL)
 	{
-		len1 = _strlen(s1);
+		len = _strlen(s1);
 	} else
-		len1 = 0;
-	ptr = (char *)malloc(sizeof(char) * (len1 + n + 1));
+		len = 0;
+	ptr = malloc(sizeof(char) * (len + n + 1));
 	if (ptr == NULL)
 		return (NULL);
+
 	concat = ptr;
+
 	while (*s1 != '\0')
 	{
 		*ptr = *s1;
 		ptr++;
 		s1++;
-	} if (*(ptr - 1) != ' ' && *s2 != ' ')
+	}
+	if (*(ptr - 1) != ' ' && *s2 != ' ')
 	{
 		*ptr = ' ';
 		ptr++;
