83505087900c90f551dcd354f47297f300b87b88
Busisa Kabi <sisakabi@icloud.com>
2023-06-19 16:29:48 -0700

    Task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,39 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_dog - prints the variables contained within the struct
+ * @d: struct containing variables
+ *
+ * Return: NULL if d == NULL
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	} else 
+	{
+		printf("Name: %s\n", d->name);
+	}
+	if (d->age < 0)
+	{
+		printf("Age: (nil)\n");
+	} else
+	{
+		printf("Age: %.6f\n", d->age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	} else 
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+
+}
