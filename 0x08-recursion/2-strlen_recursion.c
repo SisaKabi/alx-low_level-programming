d07ba3ae408d404f50b9d06ccba5ccd5c4d52af9
Busisa Kabi <sisakabi@icloud.com>
2023-06-23 18:46:43 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,18 +8,13 @@
  */
 int _strlen_recursion(char *s)
 {
-	int strlen;
-
-	strlen = 0;
-
 	if (*s == '\0')
 	{
-		return (strlen);
-	} else if (*s < '\0')
-	{
-		strlen = strlen + 1;
-		s++;
+		return (0);
+
 	}
-	_strlen_recursion(s);
-	return (strlen);
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
 }
